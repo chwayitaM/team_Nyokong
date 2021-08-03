79b75e07722d98dd5cfb89da8b82ea3bb5fbbfe7
Zainab Ikeoluwa <54579707+Zainab-ik@users.noreply.github.com>
2021-08-03 11:17:09 +0100

    Add files via upload

---
+++ Script_Zainab.js
@@ -0,0 +1,12 @@
+const myBio = () => { 
+    const name = "Zainab Ashimiyu-Abdusalam"
+    const email = "ikeoluwa933@gmail.com"
+    const slack_username = "@Zainab"
+    const biostack = "Drug Development"
+    
+    console.log(name)
+    console.log(email)
+    console.log(slack_username)
+    console.log(biostack)  }
+    
+    myBio()  
