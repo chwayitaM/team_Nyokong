2d290500513d005888659898bc1f6019050c3ca8
Nzegge <88286357+Nzegge@users.noreply.github.com>
2021-08-03 19:40:38 +0100

    I did used python
    I did used python

---
+++ Nzegge_Python.py
@@ -0,0 +1,7 @@
+#print("Name, Email, Slackname, Biostack")
+
+Name = "William NZegge"
+Email = "nzegge.william@ubuea.cm"
+Slackname = "@Nzegge"
+Biostack = "Drug Development"
+print(Name,"",Email,"",Slackname,"",Biostack)
