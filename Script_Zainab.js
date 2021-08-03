cd761d7a34797ebf35bf167030eb633e2c8f4ca5
Zainab ikeoluwa <ikeoluwa933@gmail.com>
2021-08-03 12:45:50 +0100

    js script_Zainab

--- Script_Zainab.js
+++ Script_Zainab.js
@@ -1,12 +1,15 @@
-const myBio = () => { 
+const myBio = () => {
     const name = "Zainab Ashimiyu-Abdusalam"
     const email = "ikeoluwa933@gmail.com"
-    const slack_username = "@Zainab"
-    const biostack = "Drug Development"
+    const slackUsername = "@Zainab"
+    const biostack = "Drug design and development"
     
     console.log(name)
     console.log(email)
-    console.log(slack_username)
-    console.log(biostack)  }
-    
-    myBio()  
+    console.log(slackUsername)
+    console.log(biostack)
+
+
+}
+myBio() 
+
