5a05cb8522fcaf6ab8c019df162041670696c380
Sonmaa <88287650+Sonmaa@users.noreply.github.com>
2021-08-03 23:12:46 +0100

    Add files via upload
    I used Python

---
+++ Script_Sochi.py
@@ -0,0 +1,6 @@
+#print("Name, Email, Slackname, Biostack")
+Name = "Sochi Osigwe"
+Email = "sochi.osigwe.pg87201@unn.edu.ng"
+Slackname = "@Sochi"
+Biostack = "Drug development"
+print(Name,"",Email,"",Slackname,"",Biostack)
