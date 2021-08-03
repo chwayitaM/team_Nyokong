aa38d199bb7d70c97e46534c222e829c85ef4370
prashikkk <88287081+prashikkk@users.noreply.github.com>
2021-08-03 20:08:49 +0530

    Add files via upload

---
+++ prashikkk_task_stage0.pl
@@ -0,0 +1,8 @@
+$name = "Prashik S. Chavan";
+$Mail = 'gmsbin04.prashik.chavan@gnkhalsa.edu.in';
+$Slack_username = '@prashikkk';
+$Biostack = "Drug Development and Designing";
+print("name = $name\n");
+print("Mail = $Mail\n");
+print("Slack_username = $Slack_username\n");
+print("Biostack = $Biostack\n")
