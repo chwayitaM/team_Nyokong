85489c7fd00b3dfcb3e9f4e2c3e4ab095915cb4d
Zainab ikeoluwa <ikeoluwa933@gmail.com>
2021-08-04 12:15:54 +0100

    This script is written in javascript, added to it is the hamming distance and twitter handle

--- Script_Zainab.js
+++ Script_Zainab.js
@@ -3,13 +3,29 @@ const myBio = () => {
     const email = "ikeoluwa933@gmail.com"
     const slackUsername = "@Zainab"
     const biostack = "Drug design and development"
-    
+    const twitterHandle = "@Zainab_ike"
+
+    const hammingDistance = (str1 = '', str2 = '') => {
+        if (str1.length !== str2.length) {
+            return 0;
+      }
+        let dist = 0;
+        for (let i = 0; i < str1.length; i += 1) {
+            if (str1[i] !== str2[i]) {
+                dist += 1;
+            };
+        };
+        return dist;
+    };
+
     console.log(name)
     console.log(email)
     console.log(slackUsername)
     console.log(biostack)
+    console.log(twitterHandle)
+    console.log( "Hamming distance is " + hammingDistance(slackUsername, twitterHandle))
 
 
 }
-myBio() 
+myBio()
 
