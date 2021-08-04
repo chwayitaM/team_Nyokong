15d6e90324be074730503fc886bd57b9c3958921
Chaitanya <70469756+Chaitanya1709@users.noreply.github.com>
2021-08-04 09:21:53 +0530

    This is Script written in JAVA Language.
    The package name is com.company and the class name is Chaitanya. If there are any errors running (at other systems end) the middle section can be copied and run. 
    I have checked for the syntax and other different errors there were no errors at my end at the time of uploading.

---
+++ Script_Chaitanya.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Script_Chaitanya {
+    public static void main(String[] args) {
+        String Name = "Chaitanya C. Karanjkar";
+        String Email = "ckaranjkar01@gmail.com";
+        String Slack_Username = "@Chaitanya";
+        String BioStack = "Drug Design & Development";
+        System.out.println(Name);
+        System.out.println(Email);
+        System.out.println(Slack_Username);
+        System.out.println(BioStack);
+    }
+}
