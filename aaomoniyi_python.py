a15ed68cd31b801dc3ee665353eb18a5e254c96c
Akinyemi Ademola Omoniyi <52573367+aaomoniyi@users.noreply.github.com>
2021-08-05 18:16:44 +0200

    Delete aaomoniyi_python.py

--- aaomoniyi_python.py
+++
@@ -1,8 +0,0 @@
-
-#! First assignment: print ("Name, Email, Slackname, Biostack")
-
-Name = "Akinyemi Ademola Omoniyi"
-Email = "aamomoniyi2014@gmail.com"
-Slackname = "@aaomoniyi"
-Biostack = "Drug Development"
-print(Name,"",Email,"",Slackname,"",Biostack)
