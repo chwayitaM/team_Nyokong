e3e370e9f19af2e7682d80b1735c1c6b54e247c7
Sochi <88287650+Sonmaa@users.noreply.github.com>
2021-08-06 12:41:44 +0100

    Script_Sochi_1 (Final Script)
    Hello, Please make use of the script named "Script_Sochi_1". This should be considered as my final script. I have run it, and it went perfectly well. Thanks very much

---
+++ Script_Sochi_1.py
@@ -0,0 +1,19 @@
+name = "Osigwe Sochi Chinaemerem"
+email = "sochi.osigwe.pg87201@unn.edu.ng"
+slack_username = "@Sochi"
+biostack = "Drug Development"
+twitter_handle = "@Sochi"
+
+def hammingdistance(slack_username,twitter_handle):
+	counts=0
+	if (len(slack_username)!=len(twitter_handle)):
+		counts = 0
+	else:
+		for x,(i,j) in enumerate(zip(slack_username,twitter_handle)):
+			if i!=j:
+				counts+=1
+	return counts
+
+hammingdis = hammingdistance(slack_username,twitter_handle)
+
+print(name,email,slack_username,biostack,twitter_handle,hammingdis)
