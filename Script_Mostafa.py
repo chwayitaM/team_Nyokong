45e360e366f74871f38e381ef447e89c972c43bf
MostafaHammouda <88328149+MostafaHammouda@users.noreply.github.com>
2021-08-06 15:00:21 +0200

    final python version

--- Script_Mostafa.py
+++ Script_Mostafa.py
@@ -3,21 +3,17 @@ print("mail = Mos.ahmed@nu.edu.eg \n")
 print("slack username = @Mostafa \n")
 print("biostack = drug discovery and development\n")
 
-import hashlib
+def hammingDist(twitter, slack):
+    i = 0
+    count = 0
+ 
+    while(i < len(twitter)):
+        if(twitter[i] != slack[i]):
+            count += 1
+        i += 1
+    return count
+ 
+twitter = "MostafaHammouda"
+slack = "Mos.Tafa.Ahmedd"
 
-def hamming_distance(twitter, slack):
-    return sum(t != s for t, s in zip(twitter, slack))
-
-def hamming_distance2(twitter, slack):
-    return len(list(filter(lambda x : ord(x[0])^ord(x[1]), zip(twitter, slack))))
-
-if __name__=="__main__":    
-    twitter = hashlib.md5("twitter".encode()).hexdigest()
-    slack = hashlib.md5("slack".encode()).hexdigest()
-
-    twitter = "MostafaHammouda"
-    slack = "Mos.Tafa.Ahmedd"
-
-    assert len(twitter) == len(slack)
-
-    print("haming distance =", hamming_distance(twitter, slack))
+print("haming distance =", hammingDist(twitter, slack))
