af3456e3cc5059561b47c807f1161cd210b16e8e
Zainab ikeoluwa <ikeoluwa933@gmail.com>
2021-08-06 21:59:46 +0100

    update tooutput

--- Script_Zainab.js
+++ Script_Zainab.js
@@ -8,7 +8,7 @@ const myBio = () => {
     const hammingDistance = (str1 = '', str2 = '') => {
         if (str1.length !== str2.length) {
             return 0;
-      }
+        }
         let dist = 0;
         for (let i = 0; i < str1.length; i += 1) {
             if (str1[i] !== str2[i]) {
@@ -18,12 +18,8 @@ const myBio = () => {
         return dist;
     };
 
-    console.log(name)
-    console.log(email)
-    console.log(slackUsername)
-    console.log(biostack)
-    console.log(twitterHandle)
-    console.log( "Hamming distance is " + hammingDistance(slackUsername, twitterHandle))
+    console.log(name," ",email, " " ,slackUsername, " " ,biostack," " ,twitterHandle, " " ,"Hamming distance " + hammingDistance(slackUsername, twitterHandle))
+
 
 
 }
