087db5e0352df23ace7b112bb9bc012774eb2c5b
Whoasif <88286603+Whoasif@users.noreply.github.com>
2021-08-06 23:02:16 -0700

    Update Script_asif.py
    script updated

--- Script_asif.py
+++ Script_asif.py
@@ -1,23 +1,19 @@
-print ("Name :- Asif Khan")
-print ("E-Mail ID:- khanasif84512@gmail.com")
-print ("Slack username:- @Asif")
-print ("Biostack:- Drug Discovery and Development")
-print ("twitter handle:- @whoasifkhan")
-def hammingDist(str1, str2):
+Name = "Asif Khan"
+Email = "khanasif84512@gmail.com"
+Slack_username = "@Asif"
+Biostack = "Drug Discovery and Development"
+Twitter_id = "@whoasifkhan"
+def hammingdist(Slack_username, Twitter_id):
     i = 0
     count = 0
  
-    while(i < len(str1)):
-        if(str1[i] != str2[i]):
+    while(i < len(Slack_username)):
+        if(Slack_username[i] != Twitter_id[i]):
             count += 1
         i += 1
     return count
- 
-str1 = "@Asif"
-str2 = "@whoasifkhan"
-
-print(hammingDist(str1, str2))
-
+intern_info = Name + ", " + Email + ", " + Slack_username + ", " + Biostack + ", " + Twitter_id + ", " + str(hammingdist(Slack_username, Twitter_id))
+print ( intern_info )
 
 
 
