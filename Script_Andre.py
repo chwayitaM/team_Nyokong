58a2f09a0c97151c809374b91eac5c2c950a5d21
andre-vargas-de <65573080+andre-vargas-de@users.noreply.github.com>
2021-08-07 12:02:22 -0400

    Update Script_Andre.py

--- Script_Andre.py
+++ Script_Andre.py
@@ -1,15 +1,19 @@
-def hammingDist(twitter, slack):
-    i = 0
-    count = 0
- 
-    while(i < len(twitter)):
-        if(twitter[i] != slack[i]):
-            count += 1
-        i += 1
-    return count
- 
-twitter = "AndreVargasAgu1"
-slack = "Andre"
+Name = "Andre Vargas "
+Email = "nzegge.william@ubuea.cm"
+Slack_username = "@Andre"
+Diostack = "Drug Design snd Development"
+twitter_handle = "@AndreVargasAgu1"
 
-print("Name = Andre ", ",", "mail = vargasaguilarandre@gmail.com ", ",","slack username = @Andre ", ",","biostack = drug development ", ",",
-"haming distance =", hammingDist(twitter, slack))
+def hammingdistance(slack_username,twitter_handle):
+	counts=0
+	if (len(slack_username)!=len(twitter_handle)):
+		counts = 0
+	else:
+		for x,(i,j) in enumerate(zip(slack_username,twitter_handle)):
+			if i!=j:
+				counts+=1
+	return counts
+
+hammingdis = hammingdistance(slack_username,twitter_handle)
+
+print(name,email,slack_username,biostack,twitter_handle,hammingdis)
