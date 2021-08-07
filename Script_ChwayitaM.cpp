b3621eb447bff005b63caf885d1050bb1cf6b67b
chwayitaM <88286576+chwayitaM@users.noreply.github.com>
2021-08-07 17:44:15 +0200

    c++, new  script

---
+++ Script_ChwayitaM.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char slack_handle[20]="@ChwayitaM";
+    char twitter_handle[20]="@ChwayiitaM";
+    int i,hamming_dist=0;
+    cout<<"\nChwayita Mgoboza ";
+    cout<<"chwayta99@gmail.com ";
+    cout<<"@ChwayitaM ";
+    cout<<"Drug Development ";
+    cout<<"@ChwayiitaM ";
+    for(i-0;i<9;i++)
+    {
+        if(slack_handle[i]!=twitter_handle[i])
+        {
+            hamming_dist++;
+        }
+    }
+      cout<<hamming_dist<<"\n";
+    return 0;
+}
