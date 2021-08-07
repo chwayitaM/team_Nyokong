db232a9803030877560ac55efa297e0678729fa4
fluttermaster <83166836+fluttermaster@users.noreply.github.com>
2021-08-07 22:16:36 +0200

    Add files via upload

---
+++ BashScript.sh
@@ -0,0 +1,25 @@
+echo 'name,email,slack,biostack,twitter_handle,hamming_distance' > output_file.csv
+perl Script_prashikk.pl >> output_file.csv
+node Script_Zainab.js >> output_file.csv
+python3 Script_Abdulbasit.py >> output_file.csv
+python3 Script_Andre.py >> output_file.csv
+python3 Script_Asif.py >> output_file.csv
+java Script_Chaitanya.java >> output_file.csv
+python3 script_kawthar_final.py >> output_file.csv
+dart run Script_Marwan.Dart >> output_file.csv
+Rscript Script_Moshood.R >> output_file.csv
+Rscript Script_Osmond.R >> output_file.csv
+python3 Script_Mostafa.py >> output_file.csv
+python3 Script_Nzegge.py >> output_file.csv
+python3 Script_Sochi_2_Final.py >> output_file.csv
+g++ Script_ChwayitaM.cpp
+./a.out >> output_file.csv
+g++ script_Maargavi.cpp
+./a.out >> output_file.csv
+
+
+
+
+
+
+
