082333ffbd951f71c7ed5dc698fcbdaff9d287ad
Nzegge <88286357+Nzegge@users.noreply.github.com>
2021-08-07 10:14:17 +0100

    Script_Nzegge_Final.py
    Please i have done the needful changes. Script_Nzegge_Final.py is the final copy

---
+++ Script_Nzegge_Final.py
@@ -0,0 +1,19 @@
+name = "Nzegge William"
+email = "nzegge.william@ubuea.cm"
+slack_username = "@Nzegge"
+biostack = "Drug Development"
+twitter_handle = "@Nzegge"
+
+def hammingdistance(slack_username,twitter_handle):
+	counts=0
+	if (len(slack_username)!=len(twitter_handle)):
+		counts = 0
+	else:
+		for x,(i,j) in enumerate(zip(slack_username,twitter_handle)):
+			if i!=j:
+				counts+=1
+	return counts
+
+hammingdis = hammingdistance(slack_username,twitter_handle)
+
+print(name,',',email,',',slack_username,',',biostack,',',twitter_handle,',',hammingdis)
