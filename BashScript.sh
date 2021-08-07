2ceec0a2ed92b7f72c134b444028f66c0dd938dc
Zainab ikeoluwa <ikeoluwa933@gmail.com>
2021-08-08 00:19:09 +0100

    Bash script for team Nyokong

--- BashScript.sh
+++ BashScript.sh
@@ -1,24 +1,21 @@
-$ echo 'name,email,slack,biostack,twitter_handle,hamming_distance' > output_file.csv
-$ node Script_Zainab.js >> output_file.csv
-$ dart run Script_Marwan.Dart >> output_file.csv
-$ python3 Script_Abdulbasit.py >> output_file.csv
-$ python3 Script_Andre.py >> output_file.csv
-$ python3 Script_Asif.py >> output_file.csv
-$ python3 script_kawthar_final.py >> output_file.csv
-$ python3 Script_Mostafa.py >> output_file.csv
-$ python3 Script_Nzegge.py >> output_file.csv
-$ python3 Script_Sochi_2_Final.py >> output_file.csv
-$ java Script_Chaitanya.java >> output_file.csv
-$ g++ Script_ChwayitaM.cpp
-$ ./a.out >> output_file.csv
-$ g++ script_Maargavi.cpp
-$ ./a.out >> output_file.csv
-$ Rscript Script_Osmond.R >> output_file.csv
-$ Rscript Script_Moshood.R >> output_file.csv
-$ perl Script_prashikk.pl >> output_file.csv
+#!/usr/bin/bash
+git clone https://github.com/chwayitaM/team_Nyokong.git
+cd team_Nyokong
 
 
-
-
-
-
+echo 'name,email,slack,biostack,twitter_handle,hamming_distance' > output.csv
+node Script_Zainab.js >> output.csv
+dart run Script_Marwan.Dart >> output.csv
+python3 Script_Abdulbasit.py >> output.csv
+python3 Script_Andre.py >> output.csv
+python3 Script_Asif.py >> output.csv
+python3 script_kawthar_final.py >> output.csv
+python3 Script_Mostafa.py >> output.csv
+python3 Script_Nzegge.py >> output.csv
+python3 Script_Sochi_2_Final.py >> output.csv
+java Script_Chaitanya.java >> output.csv
+g++ Script_ChwayitaM.cpp >> output.csv
+g++ script_Maargavi.cpp >> output.csv
+Rscript Script_Osmond.R >> output.csv
+Rscript Script_Moshood.R >> output.csv
+perl Script_prashikk.pl >> output.csv
