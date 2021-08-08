1a05c57bc2aab75ea29b6a7284f6830aa858428b
fluttermaster <83166836+fluttermaster@users.noreply.github.com>
2021-08-08 11:20:59 +0200

    Update Script_prashikk.pl

--- Script_prashikk.pl
+++ Script_prashikk.pl
@@ -1,6 +1,8 @@
 $name = "Prashik S. Chavan";
 $Mail = 'gmsbin04.prashik.chavan@gnkhalsa.edu.in';
 $Slack_username = '@prashikkk';
+$Biostack = "Drug Development and Designing"; $twitter_handle = "prashikkk"; 
+print("$name, $Mail, $Slack_username, $Biostack, $twitter_handle");
 $Biostack = "Drug Development and Designing"; 
 $twitter_handle = "prashikkk"; 
 print("$name, $Mail, $Slack_username, $Biostack, $twitter_handle, ");
@@ -11,6 +13,8 @@ use strict;
 my $Slack_username = 'prashikkk';
 my $twitter_handle = 'prashikkk'; 
 
+#print"hamming_distance = ", hd( $Slack_username, $twitter_handle ); # will give value 0 
 print " ", hd( $Slack_username, $twitter_handle ); # will give value 0
 
+#sub hd{ length( $[ 0 ] ) - ( ( $[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ); }
 sub hd{ length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
